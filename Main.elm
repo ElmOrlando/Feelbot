7106461c028f76de5cf082f5dcb006e873821967
Bijan <bijanbwb@gmail.com>
2017-08-28 14:06:32 -0400

    Create initial model, sample data, and view

--- Main.elm
+++ Main.elm
@@ -2,21 +2,125 @@ module Main exposing (..)
 
 import Html exposing (..)
 import Html.Attributes exposing (..)
-import Html.Events exposing (onClick)
 
 
 ---- MODEL ----
 
 
 type alias Model =
-    { message : String
+    { feels : List Feel
+    , users : List User
+    }
+
+
+type alias User =
+    { displayName : String
+    , feelCount : Int
+    , feelsExperienced : List Feel
+    , ideaCount : Int
+    , ideasCreated : List Idea
+    , username : String
+    }
+
+
+type alias Feel =
+    { emoji : String
+    , ideas : List Idea
+    , name : String
+    }
+
+
+type alias Idea =
+    { description : String
+    , votes : Int
     }
 
 
 initialModel : Model
 initialModel =
-    { message = "Hello from Elm!"
+    { feels = sampleFeelData
+    , users = sampleUserData
     }
+
+
+sampleUserData : List User
+sampleUserData =
+    [ { displayName = "Bijan"
+      , feelCount = 1
+      , feelsExperienced = []
+      , ideaCount = 1
+      , ideasCreated = []
+      , username = "bijanbwb"
+      }
+    ]
+
+
+sampleFeelData : List Feel
+sampleFeelData =
+    [ { emoji = feelEmoji Tired
+      , ideas = []
+      , name = "Tired"
+      }
+    , { emoji = feelEmoji Worried
+      , ideas = []
+      , name = "Worried"
+      }
+    , { emoji = feelEmoji Unmotivated
+      , ideas = []
+      , name = "Unmotivated"
+      }
+    ]
+
+
+type Feels
+    = Angry
+    | Confused
+    | Fearful
+    | Frustrated
+    | Overwhelmed
+    | Reluctant
+    | Sad
+    | Tired
+    | Unheard
+    | Unmotivated
+    | Worried
+
+
+feelEmoji : Feels -> String
+feelEmoji feel =
+    case feel of
+        Angry ->
+            "😠"
+
+        Confused ->
+            "😕"
+
+        Fearful ->
+            "😨"
+
+        Frustrated ->
+            "😖"
+
+        Overwhelmed ->
+            "😣"
+
+        Reluctant ->
+            "😯"
+
+        Sad ->
+            "😢"
+
+        Tired ->
+            "😫"
+
+        Unheard ->
+            "😶"
+
+        Unmotivated ->
+            "😐"
+
+        Worried ->
+            "😟"
 
 
 init : ( Model, Cmd Msg )
@@ -57,7 +161,112 @@ subscriptions model =
 view : Model -> Html Msg
 view model =
     div []
-        [ p [] [ text model.message ]
+        [ header model
+        , feelsSection model
+        , usersSection model
+        ]
+
+
+
+---- HEADER ----
+
+
+header : Model -> Html Msg
+header model =
+    div []
+        [ img [ src "https://a.slack-edge.com/ae7f/plugins/hubot/assets/service_512.png", height 60 ] []
+        , h1 [] [ text "Feelbot" ]
+        , p [] [ text introText ]
+        ]
+
+
+introText : String
+introText =
+    """
+    Feelbot is here to help.
+
+    Feelbot is like Stack Overflow for developer feelings. It's a list of
+    common emotions that developers naturally tend to experience while engaging
+    in the practice of programming.
+
+    Each "feel" has a set of user-generated "ideas" associated with it. These
+    ideas could be voted up so that particularly relevant or strong ideas
+    appear at the top.
+
+    Example: A developer clicks the "Overwhelmed" feel, and the top ideas might
+    be something along the lines of "Take a walk", "Drink some water", or
+    "Review your calendar and reduce commitments."
+    """
+
+
+
+---- FEELS ----
+
+
+feelsSection : Model -> Html Msg
+feelsSection model =
+    div []
+        [ h2 [] [ text "Feel Selector" ]
+        , p [] [ text "Experiencing a feel?" ]
+        , feelsList model.feels
+        , feelButton
+        ]
+
+
+feelsList : List Feel -> Html Msg
+feelsList feels =
+    div []
+        (List.map feelItem feels)
+
+
+feelItem : Feel -> Html Msg
+feelItem feel =
+    div []
+        [ span [] [ text (feel.emoji ++ " " ++ feel.name) ]
+        ]
+
+
+feelButton : Html Msg
+feelButton =
+    div []
+        [ p [] [ text "Want to add a feel that's not listed?" ]
+        , button [] [ text "Create a Feel" ]
+        ]
+
+
+
+---- USERS ----
+
+
+usersSection : Model -> Html Msg
+usersSection model =
+    div []
+        [ h2 [] [ text "Users" ]
+        , p [] [ text "Proudly experiencing emotions since 2017." ]
+        , usersList model.users
+        , userButton
+        ]
+
+
+usersList : List User -> Html Msg
+usersList users =
+    div []
+        (List.map userItem users)
+
+
+userItem : User -> Html Msg
+userItem user =
+    div []
+        [ p [] [ text user.displayName ]
+        , p [] [ text <| "Ideas Contributed: " ++ (toString user.ideaCount) ]
+        ]
+
+
+userButton : Html Msg
+userButton =
+    div []
+        [ p [] [ text "Want to contribute?" ]
+        , button [] [ text "Create an Account" ]
         ]
 
 
